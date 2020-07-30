e5ffc7c9d1b686d5464d7ae2641e7286e11d32fb
shravan <shravan.318@gmail.com>
2020-07-31 02:49:02 +0530

    top banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import instance from "./axios";
 import requests from "./requests";
-import "./banner.css"
+import "./banner.css";
+import { BsFillPlayFill } from "react-icons/bs";
+import {AiOutlineInfoCircle} from "react-icons/ai"
 
 function Banner() {
   const [movie, setMovie] = useState();
@@ -28,14 +30,15 @@ function Banner() {
     >
       <div className="banner__contents">
         <h1 className="banner__title">{movie?.name || movie?.title || movie?.original_name}</h1>
-        <div className="banner__buttons">
-          <button className="banner__button--white">Play</button>
-          <button className="banner__button--grey">My List</button>
-        </div>
         <h3 className="banner__description">
             {movie?.overview}
         </h3>
+        <div className="banner__buttons">
+          <button className="banner__button--white"><BsFillPlayFill className="banner__icon--play"/> Play</button>
+          <button className="banner__button--grey"><AiOutlineInfoCircle className="banner__icon--play"/>More Info</button>
+        </div>
       </div>
+      <div className="banner__bleed"></div>
     </header>
   );
 }
