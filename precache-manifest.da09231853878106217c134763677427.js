a0ee730d1919a79ee412a393e186c24544adffbd
shravan <shravan.318@gmail.com>
2020-07-31 15:30:01 +0530

    Updates

---
+++ precache-manifest.da09231853878106217c134763677427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f763c8869b4db93f71c4b0f6c19f20d",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "5c8828e0781896f3c418",
+    "url": "/netflix-clone/static/css/main.ddec0aa3.chunk.css"
+  },
+  {
+    "revision": "c26f6b700e02e269963c",
+    "url": "/netflix-clone/static/js/2.720fe38c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.720fe38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8828e0781896f3c418",
+    "url": "/netflix-clone/static/js/main.9cc621d4.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
